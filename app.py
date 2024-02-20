9187e85181c3845392d70c8298ad52d2561c6473
Javed35 <158181588+Javed35@users.noreply.github.com>
2024-02-20 13:49:01 +0800

    Update app.py
    fix bug

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask,request,render_template
 
-app=Flask(_name_)
+app=Flask(__name__)
 
 @app.route("/",methods=["get","post"])
 def index():
   return(render_template("index.html"))
 
-if_name_=="_main_":
+if __name__ == "__main__":
   app.run()
